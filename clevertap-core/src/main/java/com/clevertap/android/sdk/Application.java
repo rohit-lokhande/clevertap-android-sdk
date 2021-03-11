40be7dc1490a5eaa8923f34734d1d0d3fa64cc21
Debojyoti <debojyoti.karmakar@neebal.com>
2021-03-12 01:34:11 +0530

    Added XLog Initialization at start

--- clevertap-core/src/main/java/com/clevertap/android/sdk/Application.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/Application.java
@@ -8,8 +8,8 @@ public class Application extends android.app.Application {
 
     @Override
     public void onCreate() {
-        ActivityLifecycleCallback.register(this);
         CustomLogger.initXLog();
+        ActivityLifecycleCallback.register(this);
         super.onCreate();
     }
 }
