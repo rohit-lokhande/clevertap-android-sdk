de4ab2da8efb5a9618e32366f9dfb3c6d016306c
Rohit Lokhande <rohit.lokhande@neebal.com>
2021-03-12 00:15:01 +0530

    --remove test

--- clevertap-core/src/test/java/com/clevertap/android/sdk/pushnotification/fcm/TestFcmConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.clevertap.android.sdk.pushnotification.fcm
-
-class TestFcmConstants {
-    companion object {
-
-        const val FCM_TOKEN = "abcd"
-        const val FCM_SENDER_ID = "xyzt"
-    }
-}
